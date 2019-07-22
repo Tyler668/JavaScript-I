b1e261193972f2a9ea5afe168d8bc43024e18622
Tyler Gilchrist <tyler668@gmail.com>
2019-07-22 16:02:42 -0400

    Completed arrow syntax challenge

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -22,15 +22,18 @@ console.log(anotherFunction("Example"));
 // };
 // add(1,2);
 
-let add = function (param1, param2) {
-  return param1 + param2;
-};
-add(1,2);
+let add = (param1, param2) => param1 + param2;
+
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+let subtract = (param1, param2) => param1 - param2;
+
+console.log(subtract(1,2));
 
 
 // Stretch
@@ -40,3 +43,7 @@ add(1,2);
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map( (num) => num * 3);
+console.log(triple);
